3465c4989d7c09c26c70c45bae3cd82016c8c7f7
Flemming Eriksson <fleeri91@gmail.com>
2024-09-15 23:12:47 +0200

    test page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,40 @@
-const App = () => {
-  return <div></div>;
-};
+import CheckButton from '@/components/CheckButton'
+import CheckRow from '@/components/CheckRow'
+import CouponForm from '@/components/CouponForm'
+import { Coupon } from '@/types/Coupon'
 
-export default App;
+const CouponFormState: Coupon = {
+  rows: [
+    { '1': false, X: false, '2': false },
+    { '1': false, X: false, '2': false },
+    { '1': false, X: false, '2': false },
+    { '1': false, X: false, '2': false },
+    { '1': false, X: false, '2': false },
+    { '1': false, X: false, '2': false },
+    { '1': false, X: false, '2': false },
+    { '1': false, X: false, '2': false },
+    { '1': false, X: false, '2': false },
+    { '1': false, X: false, '2': false },
+    { '1': false, X: false, '2': false },
+    { '1': false, X: false, '2': false },
+    { '1': false, X: false, '2': false },
+  ],
+}
+
+const App = () => {
+  return (
+    <div className="mx-auto flex max-w-screen-sm items-center justify-center">
+      <CouponForm>
+        {CouponFormState.rows.map((row, index) => (
+          <CheckRow key={index}>
+            <CheckButton label="1" checked={row['1']} />
+            <CheckButton label="X" checked={row['X']} />
+            <CheckButton label="2" checked={row['2']} />
+          </CheckRow>
+        ))}
+      </CouponForm>
+    </div>
+  )
+}
+
+export default App
